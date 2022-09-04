408973b36a5cafbe4b0a837a20b2d532c9ed7f75
fardin <fardin.ebrahimzad@yahoo.com>
2022-09-04 09:50:24 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="div1">
+      <h5 class="firsthead">به برنامه مدیریت وظایف خوش آمدید</h5>
+      <div class="div2">
+        <p class="email">ایمیل</p>
+
+        <input class="inputemail" type="email" /><br></br>
+        <p class="email">رمز</p>
+        <input class="inputemail" type="password" /><br></br>
+        <p class="email">تکراررمز</p>
+        <input class="inputemail" type="password" /><br></br>
+        <br></br>
+        <br></br>
+        <button type="button" class="btn btn-success">ورود</button><br></br>
+        <a href="" class="link">ایجاد حساب کاربری جدید</a>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
