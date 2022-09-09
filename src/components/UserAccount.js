29f3900a56ee8d6cecc7af6f00645db8ab174ba0
fardin <fardin.ebrahimzad@yahoo.com>
2022-09-09 14:07:38 +0430

    adding svg file

--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -2,16 +2,40 @@ import React from 'react'
 
 export const UserAccount = () => {
   return (
-    <div class="div2">
-      <h5>ایجادحساب کاربری جدید</h5>
-      <p class="email">:ایمیل</p>
-      <input class="inputemail" type="email" /><br></br>
-      <p class="password">:رمز</p>
-      <input class="inputemail" type="password" /><br></br>
-      <p class="repeat-password">تکراررمزعبور</p>
-      <input class="inputemail" type="password" /><br></br>
-      <br></br>
-      <button type="button" class="btn btn-outline-success">ثبت</button><br></br>
+    <div>
+      <div class="div2">
+        <h6 style={{ marginBottom: "40px" }}>ایجادحساب کاربری جدید</h6>
+        <p className="email">ایمیل</p>
+        <div className="input-group mb-3">
+          <input type="email" className="form-control" aria-label="Recipient's email" aria-describedby="basic-addon2" />
+        </div>
+        <p className="password">رمزعبور</p>
+        <div className="input-group mb-3">
+          <input type="password" className="form-control" aria-label="Recipient's password" aria-describedby="basic-addon2" />
+        </div>
+        <p className="repeat-password">تکراررمزعبور</p>
+        <div className="input-group mb-3">
+          <input type="password" className="form-control" aria-label="Recipient's password" aria-describedby="basic-addon2" />
+        </div>
+        <br></br>
+        <button type="button" id="btn" className="btn btn-warning">ثبت</button>
+        <button type="button" id='btn-success' className="btn btn-outline-secondary">بازگشت</button><br></br>
+      </div>
+      <svg width="1349  " height="250" viewBox="0 0 1280 250" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_6_7)">
+          <path d="M-227 200L-189.103 195.862C-151.207 191.379 -75.634 183.621 0.601395 158.362C76.0634 133.621 151.967 91.3793 227.098 79.1379C303.333 66.3793 379.569 83.6207 454.699 104.138C530.603 125 606.065 150 682.301 141.638C757.873 133.621 833.667 91.3793 909.902 87.5C985.143 83.6207 1061.27 116.379 1136.4 120.862C1212.52 125 1287.76 100 1364 75C1439.79 50 1515.37 25 1591.6 41.6379C1667.06 58.6207 1742.97 116.379 1818.1 120.862C1894.33 125 1970.57 75 2045.7 70.8621C2121.6 66.3793 2197.07 108.621 2273.3 104.138C2348.87 100 2424.67 50 2500.9 41.6379C2576.14 33.6207 2652.27 66.3793 2727.4 100C2803.52 133.621 2878.76 166.379 2955 150C3030.79 133.621 3106.37 66.3793 3182.6 37.5C3258.06 8.62069 3333.97 16.3793 3409.1 37.5C3485.33 58.6207 3561.57 91.3793 3636.7 95.8621C3712.6 100 3788.07 75 3864.3 79.1379C3939.87 83.6207 4015.67 116.379 4091.9 133.362C4167.14 150 4243.27 150 4318.4 162.5C4394.52 175 4469.76 200 4546 204.138C4621.79 208.621 4697.37 191.379 4773.6 187.5C4849.06 183.621 4924.97 191.379 5000.1 195.862C5076.33 200 5152.57 200 5190.13 200H5227.81V250H5190.02C5152.13 250 5076.33 250 5000.1 250C4924.86 250 4848.73 250 4773.6 250C4697.48 250 4622.24 250 4546 250C4470.21 250 4394.63 250 4318.4 250C4242.94 250 4167.03 250 4091.9 250C4015.67 250 3939.43 250 3864.3 250C3788.4 250 3712.93 250 3636.7 250C3561.13 250 3485.33 250 3409.1 250C3333.86 250 3257.73 250 3182.6 250C3106.48 250 3031.24 250 2955 250C2879.21 250 2803.63 250 2727.4 250C2651.94 250 2576.03 250 2500.9 250C2424.67 250 2348.43 250 2273.3 250C2197.4 250 2121.93 250 2045.7 250C1970.13 250 1894.33 250 1818.1 250C1742.86 250 1666.73 250 1591.6 250C1515.48 250 1440.24 250 1364 250C1288.21 250 1212.63 250 1136.4 250C1060.94 250 985.033 250 909.902 250C833.667 250 757.431 250 682.301 250C606.397 250 530.935 250 454.699 250C379.127 250 303.333 250 227.098 250C151.857 250 75.732 250 0.601395 250C-75.5235 250 -150.765 250 -189.435 250H-227V200Z" fill="url(#paint0_linear_6_7)" />
+        </g>
+        <defs>
+          <linearGradient id="paint0_linear_6_7" x1="-227" y1="250" x2="-227" y2="18.6385" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#F36A3E" />
+            <stop offset="1" stop-color="#FFB30B" />
+          </linearGradient>
+          <clipPath id="clip0_6_7">
+            <rect width="1591" height="250" fill="white" transform="translate(-227)" />
+          </clipPath>
+        </defs>
+      </svg>
+
     </div>
   )
 }
